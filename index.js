b29df9238b5c18c0e2003cd371795e6f5c560344
Ahri Earl Spilner <ozogtea07@gmail.com>
2020-11-07 09:51:00 +0700

    Tugas 8 Javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+function Data() {
+  var karyawan ={
+    nik: "010001",
+    nama: "Azimatul karimah",
+    jabatan: "Staff",
+  }
+  karyawan.jabatan = "Admin"
+  console.log(karyawan);
+  console.log(karyawan.nama);
+}
+
+Data()
